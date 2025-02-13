1cadba777d549c9bb4c847af742b5ab9c58c9cc7
luisp <luis.p.vicente@gmail.com>
2025-02-13 21:59:58 +0100

    WIP

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,8 @@
-#include "myGTest.h"
+#include "modGTest.h"
 
 int main(){
-    std::cerr << myGTest::gtest_line("12890", "Hola", myGTest::COLORS.MAGENTA, myGTest::RIGHT) << std::endl;
-    std::cerr << myGTest::gtest_line("12890", "Hola", myGTest::COLORS.CYAN, myGTest::LEFT) << std::endl;
+    std::cerr << modGTest::gtest_line("12890", "Hola", modGTest::COLORS.MAGENTA, modGTest::RIGHT) << std::endl;
+    std::cerr << modGTest::gtest_line("12890", "Hola", modGTest::COLORS.CYAN, modGTest::LEFT) << std::endl;
     std::cerr << "Hola" << std::endl;
     return 0;
 }
